b4087b2ccfb14194df0927719c415d6835ff73aa
Hiroki Kihara <fj8550jf@aa.jp.fujitsu.com>
2016-05-25 14:26:12 +0900

    simple http

--- components/home/home.ts
+++ components/home/home.ts
@@ -1,13 +1,52 @@
 import {Component} from '@angular/core'
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+// Statics
+import 'rxjs/add/observable/throw';
+
+// Operators
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>My First {{name}} {{1+1}} App</h1>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.id }} - {{ hero.name }}
+      </li>
+    </ul>
     `
 })
 
 export class AppComponent {
-  public name = 'Angular';
+  private heroesUrl = "http://hirokihara.github.io/testdata/heroes.json";
+  private heroes = [];
+  errorMessage: string;
+  
+  constructor(private http: Http) {
+    this.http.get(this.heroesUrl)
+      .map(this.extractData)
+      .catch(this.handleError)
+      .subscribe(
+        heroes => this.heroes = heroes,
+        error =>  this.errorMessage = <any>error
+      );
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+  
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
 }
 　
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from '../components'
+import { HTTP_PROVIDERS } from '@angular/http';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [ HTTP_PROVIDERS ]);
